5fc84f051dc846cf0bb30031e5d049daeb3260e1
Richard Neumann <130776746+mastasalami@users.noreply.github.com>
2024-03-06 16:57:29 +0100

    Implementation of UI

--- app/src/main/java/com/example/se2_einzelbeispiel/MainActivity.kt
+++ app/src/main/java/com/example/se2_einzelbeispiel/MainActivity.kt
@@ -3,44 +3,82 @@ package com.example.se2_einzelbeispiel
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.se2_einzelbeispiel.ui.theme.SE2_EinzelbeispielTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SE2_EinzelbeispielTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                MainWindow()
             }
         }
     }
 }
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainWindow( modifier: Modifier = Modifier) {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        var matNr by rememberSaveable { mutableStateOf("") }
+        var serverResult by rememberSaveable { mutableStateOf("") }
+
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            TextField(
+                value = matNr,
+                onValueChange = { matNr = it },
+                label = { Text("Gib deine Martrikelnummer ein") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
+
+            Text(text = "Antwort vom Server: $serverResult")
+
+            Button(
+                onClick = { sendMatNrToServer(matNr) },
+            ) {
+                Text(text = "Abschicken")
+            }
+        }
+    }
+}
+
+private fun sendMatNrToServer(matNr: String) {
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     SE2_EinzelbeispielTheme {
-        Greeting("Android")
+        MainWindow()
     }
 }

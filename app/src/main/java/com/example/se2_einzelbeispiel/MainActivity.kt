0c0ab4f6a0ba358a81f649d82220c24c7281538c
Richard Neumann <130776746+mastasalami@users.noreply.github.com>
2024-03-07 16:06:11 +0100

    Added Server Call and UI update

--- app/src/main/java/com/example/se2_einzelbeispiel/MainActivity.kt
+++ app/src/main/java/com/example/se2_einzelbeispiel/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.se2_einzelbeispiel
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
@@ -23,6 +24,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.se2_einzelbeispiel.ui.theme.SE2_EinzelbeispielTheme
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.DataInputStream
+import java.io.DataOutputStream
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.io.PrintWriter
+import java.net.Socket
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
@@ -39,7 +48,7 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MainWindow( modifier: Modifier = Modifier) {
+fun MainWindow() {
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
@@ -63,7 +72,12 @@ fun MainWindow( modifier: Modifier = Modifier) {
             Text(text = "Antwort vom Server: $serverResult")
 
             Button(
-                onClick = { sendMatNrToServer(matNr) },
+                onClick = {
+                          Thread {
+                              val serverResponse = sendMatNrToServer(matNr)
+                              serverResult = serverResponse
+                          }.start()
+            },
             ) {
                 Text(text = "Abschicken")
             }
@@ -71,8 +85,38 @@ fun MainWindow( modifier: Modifier = Modifier) {
     }
 }
 
-private fun sendMatNrToServer(matNr: String) {
+private fun sendMatNrToServer(matNr: String): String {
+    var receivedData = ""
 
+    try {
+        val url = "se2-submission.aau.at"
+        val port = 20080
+        val socket = Socket(url, port)
+
+
+        //send data to server
+        val output = PrintWriter(BufferedWriter(OutputStreamWriter(socket.getOutputStream())), true)
+        output.println(matNr.toInt())
+
+        // receive data from server
+        val serverResponseReader = BufferedReader(InputStreamReader(socket.getInputStream()))
+        val response = serverResponseReader.readLine()
+
+        output.close()
+        serverResponseReader.close()
+        socket.close()
+
+        if (response.isNotEmpty()) {
+            receivedData = response
+            Log.d("main_test","Received from server: $response")
+        }
+        else
+            Log.d("main_test", "NO DATA RECEIVED!")
+        }
+    catch (e: Exception) {
+        receivedData = "ERROR: Daten konnten nicht vom Server geladen werden!"
+    }
+    return receivedData
 }
 
 @Preview(showBackground = true)
